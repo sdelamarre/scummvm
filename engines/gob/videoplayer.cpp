815c071d94909904c70f7d055f634f4eba00ce76
Simon Delamarre <simon.delamarre14@gmail.com>
2023-10-28 23:38:37 +0200

    GOB: Seek to arbitrary frame is not allowed for non-blocking videos

--- engines/gob/videoplayer.cpp
+++ engines/gob/videoplayer.cpp
@@ -510,6 +510,8 @@ bool VideoPlayer::playFrame(int slot, Properties &properties) {
 	bool primary = slot == 0;
 
 	if (video->decoder->getCurFrame() != properties.startFrame) {
+		if (video->live)
+			return true;
 
 		if (properties.startFrame != -1) {
 			// Seek into the middle of the video
